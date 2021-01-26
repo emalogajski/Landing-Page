82b139911f4ae3b68cf0c3bf1c4b466fd78cede2
Emilija Malogajski <emalogajski@gmail.com>
2021-01-26 07:18:35 +0100

    Changing variable names to be more descriptive and legible

--- js/app.js
+++ js/app.js
@@ -1,56 +1,10 @@
-let collapsible;
+let collapsibles;
 let menu;
-let sectionBig;
+let sections;
 let menuLink;
 let timer = null;
 
-
-
-function init () {
-    collapsible = document.getElementsByTagName('button');
-    menu = document.getElementById('remove');
-    sectionBig = document.getElementsByClassName('section');
-    
-
-    //Function for collapsing the sections
-    for(let i = 0; i < collapsible.length; i++) {
-        collapsible[i].addEventListener("click", function() {
-            let content = this.nextElementSibling;
-            if(content.style.display === "none") {
-                content.style.display = "block";
-                this.classList.add("sectiontitleonclick");
-            } else {
-                content.style.display = "none";
-                this.classList.remove("sectiontitleonclick");
-            }
-        })
-    }
-
-    initializeMenu();
-    menuLink = document.querySelectorAll('#remove li');
-    window.addEventListener('scroll', onScroll); 
-
-    //Function for showing and removing menu in reaction to user scrolling
-    window.addEventListener('scroll', function() {
-        if(timer !== null) {
-            clearTimeout(timer);
-            menu.classList.remove("hidden");
-        }
-
-        timer = setTimeout(function() {
-            menu.classList.add("hidden");
-        }, 2000);
-    }, false);
-
-}
-
-window.onload = init;
-
-
-//Function for showing the menu on hovering over hamburger menu as well
-hamburgerMenu = document.getElementById("menuIcon");
-
-hamburgerMenu.addEventListener('mouseover', function() {
+function showHideMenu () {
     if(timer !== null) {
         clearTimeout(timer);
         menu.classList.remove("hidden");
@@ -59,23 +13,54 @@ hamburgerMenu.addEventListener('mouseover', function() {
     timer = setTimeout(function() {
         menu.classList.add("hidden");
     }, 2000);
-}, false);
+    console.log("Hello");
+};
+
+function init () {
+    collapsibles = document.getElementsByTagName('button');
+    menu = document.getElementById('remove');
+    sections = document.getElementsByClassName('section');
+    
+
+    //Function for collapsing the sections
+    for(let i = 0; i < collapsibles.length; i++) {
+        collapsibles[i].addEventListener("click", function() {
+            this.classList.toggle('sectiontitleonclick');
+        })
+    }
+
+    initializeMenu();
+    menuLink = document.querySelectorAll('#remove li');
+    window.addEventListener('scroll', onScroll); 
+
+    //Function for showing and removing menu in reaction to user scrolling
+    window.addEventListener('scroll', showHideMenu);
+
+}
+
+window.onload = init;
+
+
+//Function for showing the menu on hovering over hamburger menu as well
+hamburgerMenu = document.getElementById("menu-icon");
+
+hamburgerMenu.addEventListener('mouseover', showHideMenu);
 
 
 //Function for the "to top" button and for adding the active state to the href attributes in the list items of the unordered list
 const onScroll = function () {
 
-    for(let i = 0; i < collapsible.length; i++) {
-        currMenuLink = menuLink[i];
-        sectionTitle = collapsible[i];
-        sectionWhole = sectionBig[i];
+    for(let i = 0; i < collapsibles.length; i++) {
+        currentMenuLink = menuLink[i];
+        sectionTitle = collapsibles[i];
+        sectionWhole = sections[i];
         const position = sectionWhole.getBoundingClientRect();
 
         if(position.top <= 0 && position.bottom >= 0) {
-            currMenuLink.classList.add('active');
+            currentMenuLink.classList.add('active');
             sectionTitle.classList.add('active');
         } else {
-            currMenuLink.classList.remove('active');
+            currentMenuLink.classList.remove('active');
             sectionTitle.classList.remove('active');
         }
 
@@ -84,8 +69,8 @@ const onScroll = function () {
 
 //Function for adding the list items
 
-function scrollToSection (collapsible) {
-    collapsible.scrollIntoView();
+function scrollToSection (collapsibles) {
+    collapsibles.scrollIntoView();
 }
 
 function clickMenu(event) {
@@ -93,17 +78,17 @@ function clickMenu(event) {
 }
 
 function initializeMenu() {
-    for (let i = 0; i < collapsible.length; i++) {
+    for (let i = 0; i < collapsibles.length; i++) {
         const li = document.createElement('li');
         const a = document.createElement('a');
         const current = document.getElementById('remove');
-        const innerText = collapsible[i].innerHTML;
+        const innerText = collapsibles[i].innerHTML;
         const stepOne = current.appendChild(li);
         const stepTwo = stepOne.appendChild(a);
         stepTwo.innerHTML = innerText;
         stepTwo.addEventListener('click', function (e) {
             e.preventDefault();
-            collapsible[i].scrollIntoView({behavior: "smooth"});
+            collapsibles[i].scrollIntoView({behavior: "smooth"});
         });
     };
 }
@@ -112,21 +97,12 @@ function initializeMenu() {
 
 if(matchMedia) {
     const mediaQueryOne = window.matchMedia('(max-width: 425px)');
-    mediaQueryOne.addEventListener('scroll', removeMenuAppear, false);
     removeMenuAppear(mediaQueryOne);
 }
 
 function removeMenuAppear(x) {
+    console.log(x);
     if(x.matches) {
-        window.removeEventListener('scroll', function() {
-            if(timer !== null) {
-                clearTimeout(timer);
-                menu.classList.remove("hidden");
-            }
-    
-            timer = setTimeout(function() {
-                menu.classList.add("hidden");
-            }, 2000);
-        });
+        window.removeEventListener('scroll', showHideMenu);
     }
 }
