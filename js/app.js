9aacbd1252c566c6d4a78d2b74f4c40d1dc85c84
Emilija Malogajski <emalogajski@gmail.com>
2021-01-28 16:16:53 +0100

    Adding media queries via JS and CSS

--- js/app.js
+++ js/app.js
@@ -2,35 +2,37 @@ let collapsibles;
 let menu;
 let sections;
 let menuLink;
+let text;
+let mediaQueryOne;
 let timer = null;
 
 function showHideMenu () {
     if(timer !== null) {
         clearTimeout(timer);
-        menu.classList.remove("hidden");
+        menu.classList.remove('hidden');
     }
 
     timer = setTimeout(function() {
-        menu.classList.add("hidden");
+        menu.classList.add('hidden');
     }, 2000);
-    console.log("Hello");
+    console.log('Hello');
 };
 
 function init () {
     collapsibles = document.getElementsByTagName('button');
-    menu = document.getElementById('remove');
+    menu = document.getElementsByClassName('menu')[0];
     sections = document.getElementsByClassName('section');
-    
 
     //Function for collapsing the sections
     for(let i = 0; i < collapsibles.length; i++) {
-        collapsibles[i].addEventListener("click", function() {
+        collapsibles[i].addEventListener('click', function() {
             this.classList.toggle('sectiontitleonclick');
+            this.nextElementSibling.classList.toggle('collapsetext');
         })
     }
 
     initializeMenu();
-    menuLink = document.querySelectorAll('#remove li');
+    menuLink = document.querySelectorAll('.menu li');
     window.addEventListener('scroll', onScroll); 
 
     //Function for showing and removing menu in reaction to user scrolling
@@ -42,7 +44,7 @@ window.onload = init;
 
 
 //Function for showing the menu on hovering over hamburger menu as well
-hamburgerMenu = document.getElementById("menu-icon");
+hamburgerMenu = document.getElementById('menu-icon');
 
 hamburgerMenu.addEventListener('mouseover', showHideMenu);
 
@@ -77,32 +79,40 @@ function clickMenu(event) {
     console.log(event);
 }
 
+function createMenuItem(menuItemText){
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.innerHTML = menuItemText;
+    li.appendChild(a);
+    return li;
+}
+
+function addScrollToSectionClickListener(menuItem, collapsibleSection){
+    menuItem.querySelector('a').addEventListener('click', function (e) {
+        e.preventDefault();
+        collapsibleSection.scrollIntoView({behavior: 'smooth'});
+    });
+}
+
 function initializeMenu() {
     for (let i = 0; i < collapsibles.length; i++) {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        const current = document.getElementById('remove');
-        const innerText = collapsibles[i].innerHTML;
-        const stepOne = current.appendChild(li);
-        const stepTwo = stepOne.appendChild(a);
-        stepTwo.innerHTML = innerText;
-        stepTwo.addEventListener('click', function (e) {
-            e.preventDefault();
-            collapsibles[i].scrollIntoView({behavior: "smooth"});
-        });
-    };
-}
-
-
-
-if(matchMedia) {
-    const mediaQueryOne = window.matchMedia('(max-width: 425px)');
-    removeMenuAppear(mediaQueryOne);
-}
-
-function removeMenuAppear(x) {
-    console.log(x);
-    if(x.matches) {
-        window.removeEventListener('scroll', showHideMenu);
+        const collapsibleSection = collapsibles[i];
+        const collapsibleSectionName = collapsibleSection.innerHTML;
+        const menuItem = createMenuItem(collapsibleSectionName);
+        addScrollToSectionClickListener(menuItem, collapsibleSection)
+        menu.appendChild(menuItem);
     }
 }
+
+window.addEventListener('resize', mediaQueries);
+
+function mediaQueries () {
+    if(window.innerWidth <= 425) {
+        window.removeEventListener('scroll', showHideMenu);
+        window.listenerAdded === false;
+    } else if(window.innerWidth > 425) {
+        window.addEventListener('scroll', showHideMenu);
+        window.listenerAdded === true;
+    }
+}
+
